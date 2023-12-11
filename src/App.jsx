9de88207a38b478c0da2a694084f42407dc52910
JosepDev <pepsafor@gmail.com>
2023-12-11 12:44:01 +0100

    otro commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import {Movies} from "../pages/MoviesGrid.jsx";
-import {Movie} from "../pages/MovieDetail.jsx";
+import { movies } from "../pages/MoviesGrid.jsx";
+import { movie } from "../pages/MovieDetail.jsx";
 import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,9 +14,8 @@ function App() {
         </header>
         <section>
           <Routes>
-            <Route path="/" element={<Navigate to="/movies" />} />
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/movie/:movie.id" element={<Movie />} />
+            <Route path="/" element={<Navigate to={movies} />} />
+            <Route path="/movie/:movie.id" element={<Navigate to={movie} />} />
           </Routes>
         </section>
       </main>
